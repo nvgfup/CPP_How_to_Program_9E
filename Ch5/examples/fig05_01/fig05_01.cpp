9115d0f51d9a8bed059d8d534a03386c1936b4c9
Poyen Chen <robertiqgood@gmail.com>
2018-11-10 01:32:52 +0800

    Update Ch5 example
    Add fig05_01.cpp

---
+++ Ch5/examples/fig05_01/fig05_01.cpp
@@ -0,0 +1,22 @@
+/*****************************************************************
+* @Author: Poyen Chen
+* @Date:   2018-11-10 01:29:44
+* @mail:   robertiqgood@gmail.com
+* @Last Modified by:   Poyen Chen
+* @Last Modified time: 2018-11-10 01:31:12
+*****************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned int counter = 1;
+
+	while(counter <= 10)
+	{
+		cout << counter << " ";
+		++counter;
+	}
+
+	cout << endl;
+}
