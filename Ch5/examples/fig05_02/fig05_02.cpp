d9acf23ca6465c00effef8b831e073b800b1cff7
Poyen Chen <robertiqgood@gmail.com>
2018-11-12 22:02:18 +0800

    Update Ch5
    Add fig05_02 in examples

---
+++ Ch5/examples/fig05_02/fig05_02.cpp
@@ -0,0 +1,16 @@
+/*****************************************************************
+* @Author: Poyen Chen
+* @Date:   2018-11-12 21:23:39
+* @mail:   robertiqgood@gmail.com
+* @Last Modified by:   Poyen Chen
+* @Last Modified time: 2018-11-12 21:24:56
+*****************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (unsigned int counter = 1; counter <= 10; ++counter)
+		cout << counter << " ";
+	cout << endl;
+}
